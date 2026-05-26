0b4175f5974c2d59ef986d710a8723ff07ccd660
TANAN <tanan@TANANs-MacBook-Air.local>
2026-05-27 01:30:58 +0300

    start on class ATM

--- src/model/ATM.java
+++ src/model/ATM.java
@@ -1,4 +1,14 @@
 package model;
 
 public class ATM {
+
+    protected String atmId;
+    protected String bankName;
+    protected String location;
+
+    public ATM(String atmId, String bankName, String location) {
+        this.atmId = atmId;
+        this.bankName = bankName;
+        this.location = location;
+    }
 }

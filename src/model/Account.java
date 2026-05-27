a3c12e04375b4624fc749f7b2ddcf73ade92bf89
Amen Endriyas <amenendriyas@gmail.com>
2026-05-27 10:55:38 +0300

    constructor + getter

--- src/model/Account.java
+++ src/model/Account.java
@@ -5,4 +5,25 @@ public class Account {
     private String ownerName;
     private double Balance;
     private String bankName;
+
+    public Account(String accountNumber, String ownerName, double Balance, String bankName){
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.Balance = Balance;
+        this.bankName = bankName;
+    }
+    public String getaccountNumber(){
+        return accountNumber;
+    }
+    public String getownerName(){
+        return ownerName;
+    }
+    public double getBalance(){
+        return Balance;
+    }
+    public String getbankName(){
+        return bankName;
+    }
+
+    
 }

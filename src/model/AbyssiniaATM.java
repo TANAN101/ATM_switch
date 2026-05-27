8c94eb3b12458a9d627b02aaa5cd832a9fafd8e0
TANAN101 <abdibezu5@gmail.com>
2026-05-28 00:37:01 +0300

    update AbyssiniaATM.java

--- src/model/AbyssiniaATM.java
+++ src/model/AbyssiniaATM.java
@@ -1,4 +1,19 @@
 package model;
 
-public class AbyssiniaATM {
+public class AbyssiniaATM extends ATM {
+
+    public AbyssiniaATM(String atmId, String location) {
+        super(atmId, "Abyssinia Bank", location);
+    }
+
+    public void processWithdrawal(Account account, double amount) {
+
+        System.out.println("Abyssinia ATM Processing...");
+
+        double fee = 15;
+
+        account.withdraw(amount, fee);
+
+        System.out.println("Abyssinia ATM fee charged: " + fee);
+    }
 }

0c56bdbaecf3d97d106b68245343b92873ccc79a
TANAN101 <abdibezu5@gmail.com>
2026-05-27 19:12:04 +0300

    update CBEBankATM.java

--- src/model/CBEBankATM.java
+++ src/model/CBEBankATM.java
@@ -1,4 +1,19 @@
 package model;
 
-public class CBEBankAccount {
+public class CBEBankATM extends ATM {
+
+    public CBEBankATM(String atmId, String location) {
+        super(atmId, "Commercial Bank of Ethiopia", location);
+    }
+
+    public void processWithdrawal(Account account, double amount) {
+
+        System.out.println("CBE ATM Processing...");
+
+        double fee = 10;
+
+        account.withdraw(amount, fee);
+
+        System.out.println("CBE ATM fee charged: " + fee);
+    }
 }

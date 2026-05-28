08d3f54eb44caa3736d914e392c327beb642d926
TANAN101 <abdibezu5@gmail.com>
2026-05-29 02:10:22 +0300

    update Transaction

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,27 +1,39 @@
 package model;
 
+
 public class Transaction {
 
     private String transactionId;
     private double amount;
     private String transactionType;
-    private String status;
+    private String status = "Status: Successful"; 
 
     public Transaction(String transactionId, double amount,
-                       String transactionType, String status) {
+                       String transactionType) {
 
         this.transactionId = transactionId;
         this.amount = amount;
         this.transactionType = transactionType;
-        this.status = status;
+        
+        
+        
     }
 
+    
     public void displayTransactionDetails() {
 
         System.out.println("===== TRANSACTION DETAILS =====");
         System.out.println("Transaction ID: " + transactionId);
         System.out.println("Amount: " + amount);
         System.out.println("Type: " + transactionType);
-        System.out.println("Status: " + status);
+        
     }
+    public void checkstatus(Account acc, double amount){
+          if (acc.getBalance() - amount > 100 ){
+           System.out.println(status);
+            
+        }else{
+            System.out.println("Status: Unsuccessful");
+        }
+    } 
 }

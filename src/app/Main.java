5f96234aba3943b8861a4bfecc8f324005fcb94d
TANAN101 <abdibezu5@gmail.com>
2026-05-28 22:08:03 +0300

    main class update

---
+++ src/app/Main.java
@@ -0,0 +1,55 @@
+package app;
+
+import model.*;
+import service.EthSwitch;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SavingsAccount account1 = new SavingsAccount(
+                "1001",
+                "Amen",
+                10000,
+                "Commercial Bank"
+        );
+
+        BusinessAccount account2 = new BusinessAccount(
+                "2001",
+                "Nathan",
+                50000,
+                "Dashen Bank"
+        );
+
+        ATM cbeATM = new CBEBankATM(
+                "ATM-CBE-01",
+                "Addis Ababa"
+        );
+
+        ATM abyssiniaATM = new AbyssiniaATM(
+                "ATM-ABY-01",
+                "Bole"
+        );
+
+        EthSwitch switchSystem = new EthSwitch();
+
+        switchSystem.validateTransaction(account1, 2000);
+
+        switchSystem.routeTransaction(
+                abyssiniaATM,
+                account1,
+                2000
+        );
+
+        account1.checkBalance();
+
+        Transaction transaction = new Transaction(
+                "TXN001",
+                2000,
+                "Withdrawal",
+                "Successful"
+        );
+
+        transaction.displayTransactionDetails();
+    }
+}

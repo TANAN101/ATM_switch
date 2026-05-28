a8609f9bad616c348ef25583f5f155db147aa062
Amen Endriyas <amenendriyas@gmail.com>
2026-05-28 21:59:59 +0300

    switch system added

--- src/model/SavingsAccount.java
+++ src/model/SavingsAccount.java
@@ -17,7 +17,7 @@ public class SavingsAccount extends Account {
 }
 
 
-}
+
 
 
 
--- src/service/ATMswitch.java
+++ src/service/ATMswitch.java
@@ -1,4 +1,18 @@
 package service;
-
+import model.Account;
+import model.ATM;
 public class ATMswitch {
+    public void validateTransaction(Account acc, double amount){
+        if (amount <= acc.getBalance()){
+            System.out.println("Transaction validated by ATMSwitch.");
+        } else{
+            System.out.println("Transaction failed: insufficient balance.");
+        }
+    }
+    public void routeTransaction(ATM atm, Account acc, double amount){
+        System.out.println("Routing transaction through ATMSwitch...");
+        atm.processWithdrawal(acc, amount);
+        atm.printReceipt();
+    }
+    
 }

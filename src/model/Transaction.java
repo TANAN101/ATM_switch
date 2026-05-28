684473767d72bfdd96dd3c035591a7894abbe328
TANAN101 <abdibezu22@gmail.com>
2026-05-28 14:05:00 +0300

    complete transaction class update

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Transaction {
+
+    private String transactionId;
+    private double amount;
+    private String transactionType;
+    private String status;
+
+    public Transaction(String transactionId, double amount,
+                       String transactionType, String status) {
+
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.status = status;
+    }
+
+    public void displayTransactionDetails() {
+
+        System.out.println("===== TRANSACTION DETAILS =====");
+        System.out.println("Transaction ID: " + transactionId);
+        System.out.println("Amount: " + amount);
+        System.out.println("Type: " + transactionType);
+        System.out.println("Status: " + status);
+    }
 }
